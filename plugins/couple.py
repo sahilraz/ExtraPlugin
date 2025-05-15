32ffda42b70722a140b45269b68b3a6ebbe4c1d2
Devil Boy <sahilraz9265@gmail.com>
2025-05-15 23:28:53 +0530

    Initial commit of this subdirectory

--- plugins/couple.py
+++ plugins/couple.py
@@ -57,128 +57,88 @@ async def ctest(_, message):
     cppic_path = "downloads/cppic.png"
 
     try:
-        is_selected = await get_couple(cid, today)
-        if not is_selected:
-            msg = await message.reply_text("❣️")
-            list_of_users = []
+        msg = await message.reply_text("❣️")
+        list_of_users = []
 
-            async for i in app.get_chat_members(message.chat.id, limit=50):
-                if not i.user.is_bot and not i.user.is_deleted:
-                    list_of_users.append(i.user.id)
+        async for i in app.get_chat_members(message.chat.id, limit=50):
+            if not i.user.is_bot and not i.user.is_deleted:
+                list_of_users.append(i.user.id)
 
+        c1_id = random.choice(list_of_users)
+        c2_id = random.choice(list_of_users)
+        while c1_id == c2_id:
             c1_id = random.choice(list_of_users)
-            c2_id = random.choice(list_of_users)
-            while c1_id == c2_id:
-                c1_id = random.choice(list_of_users)
 
-            photo1 = (await app.get_chat(c1_id)).photo
-            photo2 = (await app.get_chat(c2_id)).photo
+        photo1 = (await app.get_chat(c1_id)).photo
+        photo2 = (await app.get_chat(c2_id)).photo
 
-            N1 = (await app.get_users(c1_id)).mention
-            N2 = (await app.get_users(c2_id)).mention
+        N1 = (await app.get_users(c1_id)).mention
+        N2 = (await app.get_users(c2_id)).mention
 
-            try:
-                p1 = await app.download_media(photo1.big_file_id, file_name=p1_path)
-            except Exception:
-                p1 = download_image(
-                    "https://telegra.ph/file/05aa686cf52fc666184bf.jpg", p1_path
-                )
-            try:
-                p2 = await app.download_media(photo2.big_file_id, file_name=p2_path)
-            except Exception:
-                p2 = download_image(
-                    "https://telegra.ph/file/05aa686cf52fc666184bf.jpg", p2_path
-                )
-
-            img1 = Image.open(p1)
-            img2 = Image.open(p2)
-
-            background_image_path = download_image(
-                "https://te.legra.ph/file/d1231b04cde4b1e31cef8-83b917381561a4f48a.jpg", cppic_path
+        try:
+            p1 = await app.download_media(photo1.big_file_id, file_name=p1_path)
+        except Exception:
+            p1 = download_image(
+                "https://telegra.ph/file/05aa686cf52fc666184bf.jpg", p1_path
             )
-            img = Image.open(background_image_path)
+        try:
+            p2 = await app.download_media(photo2.big_file_id, file_name=p2_path)
+        except Exception:
+            p2 = download_image(
+                "https://telegra.ph/file/05aa686cf52fc666184bf.jpg", p2_path
+            )
 
-            img1 = img1.resize((437, 437))
-            img2 = img2.resize((437, 437))
+        img1 = Image.open(p1)
+        img2 = Image.open(p2)
 
-            mask = Image.new("L", img1.size, 0)
-            draw = ImageDraw.Draw(mask)
-            draw.ellipse((0, 0) + img1.size, fill=255)
+        background_image_path = download_image(
+            "https://te.legra.ph/file/d1231b04cde4b1e31cef8-83b917381561a4f48a.jpg", cppic_path
+        )
+        img = Image.open(background_image_path)
 
-            mask1 = Image.new("L", img2.size, 0)
-            draw = ImageDraw.Draw(mask1)
-            draw.ellipse((0, 0) + img2.size, fill=255)
+        img1 = img1.resize((437, 437))
+        img2 = img2.resize((437, 437))
 
-            img1.putalpha(mask)
-            img2.putalpha(mask1)
+        mask = Image.new("L", img1.size, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0) + img1.size, fill=255)
 
-            draw = ImageDraw.Draw(img)
+        mask1 = Image.new("L", img2.size, 0)
+        draw = ImageDraw.Draw(mask1)
+        draw.ellipse((0, 0) + img2.size, fill=255)
 
-            img.paste(img1, (116, 160), img1)
-            img.paste(img2, (789, 160), img2)
+        img1.putalpha(mask)
+        img2.putalpha(mask1)
 
-            img.save(test_image_path)
+        draw = ImageDraw.Draw(img)
 
-            TXT = f"""
+        img.paste(img1, (116, 160), img1)
+        img.paste(img2, (789, 160), img2)
+
+        img.save(test_image_path)
+
+        TXT = f"""
 **Tᴏᴅᴀʏ's ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ:
 
-{N1} + {N2} = 💚
+{N1} + {N2} = 💚**
+        """
 
-Nᴇxᴛ ᴄᴏᴜᴘʟᴇs ᴡɪʟʟ ʙᴇ sᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow}!!**
-            """
-
-            await message.reply_photo(
-                test_image_path,
-                caption=TXT,
-                reply_markup=InlineKeyboardMarkup(
+        await message.reply_photo(
+            test_image_path,
+            caption=TXT,
+            reply_markup=InlineKeyboardMarkup(
+                [
                     [
-                        [
-                            InlineKeyboardButton(
-                                text="Aᴅᴅ ᴍᴇ 🌋",
-                                url=f"https://t.me/{app.username}?startgroup=true",
-                            )
-                        ]
+                        InlineKeyboardButton(
+                            text="Aᴅᴅ ᴍᴇ 🌋",
+                            url=f"https://t.me/{app.username}?startgroup=true",
+                        )
                     ]
-                ),
-            )
+                ]
+            ),
+        )
 
-            await msg.delete()
-            a = upload_file(test_image_path)
-            for x in a:
-                img_url = "https://graph.org/" + x
-                couple = {"c1_id": c1_id, "c2_id": c2_id}
-                await save_couple(cid, today, couple, img_url)
-
-        else:
-            msg = await message.reply_text("❣️")
-            b = await get_image(cid)
-            c1_id = int(is_selected["c1_id"])
-            c2_id = int(is_selected["c2_id"])
-            c1_name = (await app.get_users(c1_id)).first_name
-            c2_name = (await app.get_users(c2_id)).first_name
-
-            TXT = f"""
-**Tᴏᴅᴀʏ's ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ 🎉:
-
-[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❣️
-
-Nᴇxᴛ ᴄᴏᴜᴘʟᴇs ᴡɪʟʟ ʙᴇ sᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow}!!**
-            """
-            await message.reply_photo(
-                b,
-                caption=TXT,
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(
-                                text="Aᴅᴅ ᴍᴇ🌋",
-                                url=f"https://t.me/{app.username}?startgroup=true",
-                            )
-                        ]
-                    ]
-                ),
-            )
-            await msg.delete()
+        await msg.delete()
 
     except Exception as e:
         print(str(e))
